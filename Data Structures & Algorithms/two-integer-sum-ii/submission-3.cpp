9a222113ed9c6fcfbea114031a0f2e9d803a1b19
Prasanna005118 <83009366+Prasanna005118@users.noreply.github.com>
2026-04-28 23:57:53 +0530

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> res;
+        int left = 0;
+        int right = numbers.size() - 1;
+
+        while(left<right)
+        {
+            int sum = numbers[left]+numbers[right];
+            if(sum == target)
+            {
+                res.push_back(++left);
+                res.push_back(++right);
+            }
+            if(sum < target)
+                left++;
+            else
+                right--;
+        }
+        return res;
+    }
+};
